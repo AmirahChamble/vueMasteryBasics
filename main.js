ae9771d09dee54973dc155c1e2fc152239ae446e
Amirah Chamble <amirahchamble@Amirahs-MBP.fios-router.home>
2018-12-24 10:13:48 -0500

    feat: added basic form

--- main.js
+++ main.js
@@ -6,52 +6,53 @@ Vue.component('product',{
         }
     },
     template:   `
-    <div>
-    <div class="product">
-    <h1 class="page-title"> {{ title }} </h1>
-</div>
+                <div>
+                    <div class="product">
+                        <h1 class="page-title"> {{ title }} </h1>
+                    </div>
 
-<div class="product-section">
+                    <div class="product-section">
 
-    <div class="product-image">
-        <img v-bind:src="image"/>
+                         <div class="product-image">
+                            <img v-bind:src="image"/>
 
-        <div class="variants">
-            <div 
-                v-for="(variant, index) in variants" 
-                :key="variant.id"
-                class="variants-options"
-                v-bind:style = "{backgroundColor: variant.color}"
-                @mouseover="updateProduct(index)"
-            ></div>
-        </div>
-    </div>
+                                <div class="variants">
+                                    <div 
+                                        v-for="(variant, index) in variants" 
+                                        :key="variant.id"
+                                        class="variants-options"
+                                        v-bind:style = "{backgroundColor: variant.color}"
+                                        @mouseover="updateProduct(index)"
+                                    ></div>
+                                </div>
+                        </div>
 
-    <div class="product-info">
-        <h1 > {{ product }} ( {{ variants[selectedVariant].color }} ) </h1>
-        <p v-if="variants[selectedVariant].quantity > 10 ">In Stock</p>
-        <p v-else-if="10 >= variants[selectedVariant].quantity && variants[selectedVariant].quantity > 0 "> Almost Sold Out !</p>
-        <p v-else>Out of Stock</p>
-        <p>Premium Member : {{ premium  }}</p>
-        <p v-if="premium"> Shipping is $2.99 </p>
-        <p v-else> Shipping is $4.99 </p>
+                        <div class="product-info">
+                            <h1 > {{ product }} ( {{ variants[selectedVariant].color }} ) </h1>
+                            <p v-if="variants[selectedVariant].quantity > 10 ">In Stock</p>
+                            <p v-else-if="10 >= variants[selectedVariant].quantity && variants[selectedVariant].quantity > 0 "> Almost Sold Out !</p>
+                            <p v-else>Out of Stock</p>
+                            <p>Premium Member : {{ premium  }}</p>
+                            <p v-if="premium"> Shipping is $2.99 </p>
+                            <p v-else> Shipping is $4.99 </p>
 
-        <ul>
-            <li v-for="detail in details"> {{ detail }} </li>
-        </ul>
+                            <ul>
+                                <li v-for="detail in details"> {{ detail }} </li>
+                            </ul>
 
 
-        <div class="cart">
-            <button 
-                @click="addToCart"
-                v-bind:disabled ="!inStock"
-            > Add to Cart </button>
+                            <div class="cart">
+                                <button 
+                                    @click="addToCart"
+                                    v-bind:disabled ="!inStock"
+                                > Add to Cart </button>
            
             
-        </div>
-    </div>
-</div>
-    </div>
+                            </div>
+                        </div>
+                    </div>
+                    <product-review/>
+                </div>
     `,
     data(){
         return {
@@ -102,6 +103,45 @@ Vue.component('product',{
     },
 })
 
+Vue.component('product-review',{
+    template: ` 
+        <form class="product-form">
+            <p>
+                <label for="name" > Name: </label>
+                <input id="name" v-model="name">
+            </p>
+
+            <p>
+                <label for="review" > Review: </label>
+                <textarea id="review" v-model="review"/>
+            </p>
+
+            <p>
+                <label for="rating"> Rating: </label>
+                <select id="rating" v-model="rating"> 
+                    <option value="5" > 5 </option>
+                    <option value="4" > 4 </option>
+                    <option value="3" > 3 </option>
+                    <option value="2" > 2 </option>
+                    <option value="1" > 1 </option>
+                    <option value="0" > 0 </option>
+                </select>
+            </p>
+
+            <p>
+                <input id="submit" type="submit" value="submit"/>
+            </p>
+
+        </form>
+    `,
+    data(){
+        return{
+            name: " ",
+            review: " ",
+            rating: " "
+        }
+    }
+})
 
 var app = new Vue ({
     el: "#app",
